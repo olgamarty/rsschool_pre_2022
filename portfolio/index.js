b8134a9208e867759e2cef69c420cb154f3dad08
OlgaMarty <olalett@mail.ru>
2022-01-31 22:44:49 +0300

    feat: add effects to buttons and the function of saving info to Local Storage

--- portfolio/index.js
+++ portfolio/index.js
@@ -2,7 +2,7 @@
 
 import i18Obj from './translate.js';
 
-const body = document.querySelector('body');
+const body = document.querySelector('.body');
 const iconBurger = document.querySelector('.icon-burger');
 const headerNav = document.querySelector('.header-nav');
 const darkness = document.querySelector('.darkness');
@@ -11,6 +11,12 @@ const btnToggle = portfolioBtns.querySelectorAll('.btn-toggle');
 const collageItems = document.querySelectorAll('.collage-item');
 const langButtons = document.querySelector('.languages-container');
 const langButton = langButtons.querySelectorAll('.lang-btn');
+const themeButton = body.querySelector('.theme-btn');
+const sectionTitles = body.querySelectorAll('.section-title');
+const sectionTitleWrappers = body.querySelectorAll('.section-title__wrapper');
+
+let language = 'en';
+let theme = 'dark';
 
 
 iconBurger.addEventListener('click', function () {
@@ -28,7 +34,6 @@ function closeMenu(event) {
 		body.classList.remove('lock');
 	}
 }
-
 headerNav.addEventListener('click', closeMenu);
 
 function changeImage(event) {
@@ -44,6 +49,15 @@ function changeImage(event) {
 
 portfolioBtns.addEventListener('click', changeImage);
 
+function preloadSummerImages() {
+	for (let i = 1; i <= 6; i++) {
+		const img = new Image();
+		const seasons = ['winter', 'spring', 'summer', 'autumn'];
+		seasons.forEach(el => img.src = `./assets/img/${el}/${i}.jpg`);
+	}
+}
+preloadSummerImages();
+
 
 function getTranslate(lang) {
 	const internationalization = document.querySelectorAll('[data-i18n]');
@@ -55,39 +69,108 @@ function getTranslate(lang) {
 				el.textContent = ''
 			}
 		}
-
 	})
+	language = lang;
+	if (lang === 'ru') {
+		langButtons.querySelector('.ru-lang').classList.add('active-lang');
+		langButtons.querySelector('.en-lang').classList.remove('active-lang');
+	}
 }
 
 langButtons.addEventListener('click', function (event) {
 	getTranslate(event.target.dataset.lang);
 	langButton.forEach(el => el.classList.remove('active-lang'));
 	event.target.classList.add('active-lang');
+});
+
+
+const themeElementsArr = [btnToggle, sectionTitles];
+
+themeButton.addEventListener('click', function () {
+	themeElementsArr.forEach(el => {
+		el.forEach(elem => {
+			elem.classList.toggle('light-theme');
+		})
+	})
+
+	body.classList.toggle('light-theme');
+
+	sectionTitleWrappers.forEach(el => {
+		el.classList.toggle('section-title__wrapper_theme');
+	})
+	console.log(body.classList.contains('light-theme'));
+	body.classList.contains('light-theme') ? theme = 'light' : theme = 'dark';
 })
+
+function turnOnLightTheme(themes) {
+	if (themes === 'light') {
+		themeElementsArr.forEach(el => {
+			el.forEach(elem => {
+				elem.classList.add('light-theme');
+			})
+		})
+
+		body.classList.add('light-theme');
+
+		sectionTitleWrappers.forEach(el => {
+			el.classList.add('section-title__wrapper_theme');
+		})
+		theme = 'light';
+	} else {
+		themeElementsArr.forEach(el => {
+			el.forEach(elem => {
+				elem.classList.remove('light-theme');
+			})
+		})
+
+		body.classList.remove('light-theme');
+
+		sectionTitleWrappers.forEach(el => {
+			el.classList.remove('section-title__wrapper_theme');
+		})
+		theme = 'dark';
+	}
+}
+
+
+function setLocalStorage() {
+	localStorage.setItem('language', language);
+	localStorage.setItem('theme', theme);
+}
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+	if (localStorage.getItem('language')) {
+		const language = localStorage.getItem('language');
+		getTranslate(language);
+	}
+	if (localStorage.getItem('theme')) {
+		const theme = localStorage.getItem('theme');
+		turnOnLightTheme(theme);
+	}
+}
+window.addEventListener('load', getLocalStorage)
+
+
+
 
 
 
 console.log(`
-Результат: 85 баллов.
-1. Верстка соответствует макету. Ширина экрана 768px +48
-   - блок <header> +6
-   - секция hero +6
-   - секция skills +6
-   - секция portfolio +6
-   - секция video +6
-   - секция price +6
-   - секция contacts +6
-   - блок <footer> +6
-2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15
-   - нет полосы прокрутки при ширине страницы от 1440рх до 768рх +5
-   - нет полосы прокрутки при ширине страницы от 768рх до 480рх +5
-   - нет полосы прокрутки при ширине страницы от 480рх до 320рх +5
-3. На ширине экрана 768рх и меньше реализовано адаптивное меню +22
-   - при ширине страницы 768рх панель навигации скрывается, появляется бургер-иконка +2
-   - при нажатии на бургер-иконку справа плавно появляется адаптивное меню, бургер-иконка изменяется на крестик +4
-   - высота адаптивного меню занимает всю высоту экрана. При ширине экрана 768-620рх вёрстка меню соответствует макету, когда экран становится уже, меню занимает всю ширину экрана +4
-   - при нажатии на крестик адаптивное меню плавно скрывается уезжая за правую часть экрана, крестик превращается в бургер-иконку +4
-   - бургер-иконка, которая при клике превращается в крестик, создана при помощи css-анимаций без использования изображений +2
-   - ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +2
-   - при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, крестик превращается в бургер-иконку +4
+1. Смена изображений в секции portfolio +25
+ - при кликах по кнопкам Winter, Spring, Summer, Autumn в секции portfolio отображаются изображения из папки с соответствующим названием +20
+ - кнопка, по которой кликнули, становится активной т.е. выделяется стилем. Другие кнопки при этом будут неактивными +5
+2. Перевод страницы на два языка +25
+ - при клике по надписи ru англоязычная страница переводится на русский язык +10
+ - при клике по надписи en русскоязычная страница переводится на английский язык +10
+ - надписи en или ru, соответствующие текущему языку страницы, становятся активными т.е. выделяются стилем +5
+3. Переключение светлой и тёмной темы +25
+Внешний вид тёмной темы соответствует макету, который верстали в предыдущих частях задания, внешний вид светлой темы соответствует одному из двух вариантов макетов на выбор.
+На страницу добавлен переключатель при клике по которому:
+ - тёмная тема приложения сменяется светлой +10
+ - светлая тема приложения сменяется тёмной +10
+ - после смены светлой и тёмной темы интерактивные элементы по-прежнему изменяют внешний вид при наведении и клике и при этом остаются видимыми на странице (нет ситуации с белым шрифтом на белом фоне) +5
+Дополнительный функционал: выбранный пользователем язык отображения страницы и светлая или тёмная тема сохраняются при перезагрузке страницы +5
+Дополнительный функционал: сложные эффекты для кнопок при наведении и/или клике +5
+Для получения максимального балла за пункт требований достаточно добавить кнопкам только один эффект.
 `);
